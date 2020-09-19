7bac7a31630bfce29ae0795229e77853a03d2fa3
Sater <LiteSater@protonmail.ch>
2020-09-19 13:28:01 +0200

    Update memory.h
    Added TeamColor index containing colors for the classes.
    Index is parallel to TeamName index.

--- memory.h
+++ memory.h
@@ -17,3 +17,4 @@ pointer camera;
 std::vector<Entity> entityList;
 std::vector<Entity> locationList;
 char TeamName[18][50] = { "SCP-173", "Class-D Personnel", "Spectator", "SCP-106", "Nine-Tailed Fox Scientist", "SCP-049", "Scientist", "SCP-079", "Chaos Insurgency", "SCP-096", "SCP-049-2", "Nine-Tailed Fox Lieutenant", "Nine-Tailed Fox Commander", "Nine-Tailed Fox Cadet", "TUTORIAL", "Facility Guard", "SCP-939-53", "SCP-939-89", };
+char TeamColor[18][50] = { "#ff1900", "#ffece6", "white", "#cc0000", "#4d4dff", "red", "yellow", "#ff3333", "#33cc33", "red", "#ff6666", "#3333ff", "#1a1aff", "6666ff", "white", "lightblue", "#ff3364", "#ff3364", };
